1d69151cda08b2a7f42a45b8554ec338408c8e39
Anthony Sottile <asottile@umich.edu>
2018-03-25 17:16:59 -0700

    v1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'golang.'
     ),
     url='https://github.com/asottile/setuptools-golang',
-    version='1.2.0',
+    version='1.3.0',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
